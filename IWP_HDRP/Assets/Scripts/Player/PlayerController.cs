0a89a59c303404a34a07a286a7dd85b2fb9c14e8
V3lric <46673393+V3lric@users.noreply.github.com>
2023-11-23 03:19:15 +0800

    Revamped movement + cam movement

--- IWP_HDRP/Assets/Scripts/Player/PlayerController.cs
+++ IWP_HDRP/Assets/Scripts/Player/PlayerController.cs
@@ -8,19 +8,14 @@ public class PlayerController : MonoBehaviour
     public bool disabled = false;
 
     [Header("Player Stats")]
-    [SerializeField] int walkSpeed;
+    public float speed = 10f;
     [SerializeField] int talentPt,coins;
 
     [Header("Movement Stats")]
-    public float maxForwardSpeed = 5f;
-    public float maxBackwardSpeed = 4f;
-    public float acceleration = 20.0f;
-    public float maxRotateSpeed = 150f;
-    public float rotateAcceleration = 600f;
-    protected Vector3 directSpeed;
-    protected float speed;
-    protected float rotateSpeed;
-    protected Vector3 moveDelta = Vector3.zero;
+    Vector2 movement;
+    public float turnSmoothTime = 0.1f;
+    [SerializeField] float turnSmoothVelocity;
+    public Transform Cam;
 
     [Header("Jump Stats")]
     public bool airborne;
@@ -29,10 +24,12 @@ public class PlayerController : MonoBehaviour
     protected int maxJumpsInAir = 1;
     public float jumpSpeed = 20f;
     public float jumpDelay = 0.1f;
+    public float jumpheight;
+    Vector3 Velocity;
     public float gravity = 80f;
     CharacterController characterController;
 
-    public bool Grounded = true;//check if frounded
+    public bool Grounded = true;//check if grounded
     public float GroundedOffset = -0.14f;
     public float GroundedRadius = 0.32f;//radius of groundcheck
     public LayerMask Floor;//what layer player can walk on
@@ -41,12 +38,16 @@ public class PlayerController : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
         pData = GameObject.FindGameObjectWithTag("Data").GetComponent<PlayerData>();
-        walkSpeed = pData.GetWalkSpeed();
+        speed = pData.GetWalkSpeed();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         if (!disabled)
         {
             {
@@ -59,93 +60,72 @@ public class PlayerController : MonoBehaviour
 
     public void Movement()//WASD
     {
-        // Calculate direct speed and speed.
-        var right = Vector3.right;
-        var forward = Vector3.forward;
-        if (Camera.main)
+
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector3 direction = new Vector3(movement.x, 0f, movement.y).normalized;
+
+        if (direction.magnitude >= 0.1f)
         {
-            right = Camera.main.transform.right;
-            right.y = 0.0f;
-            right.Normalize();
-            forward = Camera.main.transform.forward;
-            forward.y = 0.0f;
-            forward.Normalize();
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-
-        var targetSpeed = right * Input.GetAxisRaw("Horizontal");
-        targetSpeed += forward * Input.GetAxisRaw("Vertical");
-        if (targetSpeed.sqrMagnitude > 0.0f)
-        {
-            targetSpeed.Normalize();
-        }
-        targetSpeed *= maxForwardSpeed;
-
-        var speedDiff = targetSpeed - directSpeed;
-        if (speedDiff.sqrMagnitude < acceleration * acceleration * Time.deltaTime * Time.deltaTime)
-        {
-            directSpeed = targetSpeed;
-        }
-        else if (speedDiff.sqrMagnitude > 0.0f)
-        {
-            speedDiff.Normalize();
-
-            directSpeed += speedDiff * acceleration * Time.deltaTime;
-        }
-        speed = directSpeed.magnitude;
-
-        // Calculate rotation speed - ignore rotate acceleration.
-        rotateSpeed = 0.0f;
-        if (targetSpeed.sqrMagnitude > 0.0f)
-        {
-            var localTargetSpeed = transform.InverseTransformDirection(targetSpeed);
-            var angleDiff = Vector3.SignedAngle(Vector3.forward, localTargetSpeed.normalized, Vector3.up);
-
-            if (angleDiff > 0.0f)
-            {
-                rotateSpeed = maxRotateSpeed;
-            }
-            else if (angleDiff < 0.0f)
-            {
-                rotateSpeed = -maxRotateSpeed;
-            }
-
-            // Assumes that x > NaN is false - otherwise we need to guard against Time.deltaTime being zero.
-            if (Mathf.Abs(rotateSpeed) > Mathf.Abs(angleDiff) / Time.deltaTime)
-            {
-                rotateSpeed = angleDiff / Time.deltaTime;
-            }
-        }
-        moveDelta = new Vector3(directSpeed.x, moveDelta.y, directSpeed.z);
-
-        // Calculate move delta.
-        characterController.Move(directSpeed * (speed * Time.deltaTime) + new Vector3(0.0f, moveDelta.y, 0.0f) * Time.deltaTime);
     }
 
     public void Gravity()//set gravity on player
     {
-        //Check if player is grounded.
+        //new jump code
         if (Grounded)
-        {
-            jumpsInAir = maxJumpsInAir;
-        }
-        else
-            moveDelta.y -= gravity * Time.deltaTime;
-        // Check if player is jumping.
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (!airborne || jumpsInAir > 0)
-            {
-                if (airborne)
-                {
-                    jumpsInAir--;
-                }
+            Velocity.y = -1;
 
-                moveDelta.y = jumpSpeed;
-
-                airborne = true;
-                airborneTime = jumpDelay;
-            }
+        if (Input.GetButtonDown("Jump") && Grounded)
+        {
+            Velocity.y += Mathf.Sqrt((jumpSpeed * 10) * -2f * gravity);
         }
+        if (Velocity.y > -20)
+            Velocity.y += (gravity * 10) * Time.deltaTime;
+
+        characterController.Move(Velocity * Time.deltaTime);
+
+        //// Check if player is grounded.
+        //if (Grounded)
+        //{
+        //    jumpsInAir = maxJumpsInAir;
+        //}
+        //else
+        //{
+        //    moveDelta.y -= gravity * Time.deltaTime;
+        //}
+
+        //// Check if player is jumping.
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    if (!airborne || jumpsInAir > 0)
+        //    {
+        //        if (airborne)
+        //        {
+        //            jumpsInAir--;
+        //        }
+
+        //        moveDelta.y = jumpSpeed;
+
+        //        airborne = true;
+        //        airborneTime = jumpDelay;
+        //    }
+        //}
+
+        //// Apply airborne time
+        //if (airborneTime > 0)
+        //{
+        //    airborneTime -= Time.deltaTime;
+        //}
+        //else
+        //{
+        //    airborne = false;
+        //}
     }
     private void CheckGrounded()
     {
