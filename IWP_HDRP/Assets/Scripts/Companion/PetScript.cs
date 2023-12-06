f61cd6115f037a1b54d3a8e7725ff0633769ee9f
V3lric <46673393+V3lric@users.noreply.github.com>
2023-12-07 02:08:47 +0800

    slowed speed in testscene

--- IWP_HDRP/Assets/Scripts/Companion/PetScript.cs
+++ IWP_HDRP/Assets/Scripts/Companion/PetScript.cs
@@ -16,6 +16,7 @@ public class PetScript : MonoBehaviour
         ai.destination = dest;
         if (ai.remainingDistance <= ai.stoppingDistance)
         {
+            //ai.destination = enemy.position + (transform.position - enemy.position).normalized * desiredDistance;
             //aiAnim.ResetTrigger("jog");
             //aiAnim.SetTrigger("idle");
         }
