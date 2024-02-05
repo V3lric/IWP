48f440c4caaea8558adaa8b7a7eccc429427bc6a
V3lric <46673393+V3lric@users.noreply.github.com>
2024-02-06 02:10:34 +0800

    added more particles and added last npc

--- IWP_HDRP/Assets/Scripts/GamePlay/SceneBossRun/FallingWall.cs
+++ IWP_HDRP/Assets/Scripts/GamePlay/SceneBossRun/FallingWall.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class FallingWall : MonoBehaviour
 {
     public Animator animator;
+    public ParticleSystem particleBurst;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             animator.SetTrigger("Fall");
+            StartCoroutine(Slam());
         }
+    }
+
+    IEnumerator Slam()
+    {
+        yield return new WaitForSeconds(1.5f);
+        particleBurst.Play();
     }
 }
